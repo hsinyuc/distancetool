a39c6d5dcaf4eb4b0af258707b8d7869539f57e1
Hsin-Yu Chen <hsinyuchen@uchicago.edu>
2017-01-26 17:52:50 -0600

    initialize the directory

---
+++ codes/find_horizon_range.py
@@ -0,0 +1,153 @@
+"""
+Calculate the horizon and range for a given 
+binary. The horizon is defined as the distance at which a face-on and overhead (ideally located) binary is detected 
+with SNR=snr_threshold with a single IFO. It represents the farthest this binary could be detected above threshold. 
+There are two definitions of range being discussed at this point, 
+
+[1]
+Detectable comoving volume--V_detect
+range=(V_detect*3/4/pi)^(1/3)
+
+[2]
+Take the value in [1], that we assume to be a comoving distance, and convert it to the luminosity distance.
+
+Usage:
+
+find_horizon_range(m1,m2,asdfile,approx=ls.IMRPhenomD)
+
+Input--
+m1,m2: binary component masses in solar mass
+asdfile: noise curve file [frequency, strain]
+
+Optional inputs--
+approx: frequency domain waveform. Default is IMRPhenomD.
+
+Output--
+Horizon redshift,Horizon luminosity distance (Mpc), range defined in [1] (Mpc), range defined in [2] (Mpc), detectable comoving volume (Mpc^3)
+
+Author: 
+Hsin-Yu Chen (hsin-yu.chen@ligo.org)
+
+"""
+
+
+from numpy import *
+import cosmolopy.distance as cd
+import lal
+import lalsimulation as ls
+from scipy.optimize import leastsq
+from scipy.interpolate import interp1d
+
+global snr_th,cosmo
+snr_th=8.
+cosmo = {'omega_M_0':0.308, 'omega_lambda_0':0.692, 'omega_k_0':0.0, 'h':0.678}
+
+##find redshift for a given luminosity distance
+def findzfromDL(z,DL):
+    return DL-cd.luminosity_distance(z, **cosmo)
+    
+##find redshift for a given luminosity distance
+def findzfromDC(z,DC):
+    return DC-cd.comoving_distance(z, **cosmo)
+    
+##estimate the horizon for recursive evaluation in the main code
+def horizon_dist_eval(orig_dist,snr,z0):
+    guess_dist=orig_dist*snr/snr_th
+    guess_redshift,res=leastsq(findzfromDL,z0,args=guess_dist)
+    return guess_redshift[0],guess_dist
+
+##generate the waveform    
+def get_htildas(m1,m2,dist,
+		   phase=0., 
+		   df=1e-2,  
+		   s1x=0.0, 
+		   s1y=0.0, 
+		   s1z=0.0, 
+		   s2x=0.0, 
+		   s2y=0.0, 
+		   s2z=0.0, 
+		   fmin=10.,
+		   fmax=0.,
+		   fref=10.,  
+		   iota=0., 
+		   lambda1=0., 
+		   lambda2=0., 
+		   waveFlags=None, 
+		   nonGRparams=None, 
+		   amp_order=0, 
+		   phase_order=-1, 
+		   approx=ls.IMRPhenomD,
+		   ):
+	hplus_tilda, hcross_tilda = ls.SimInspiralChooseFDWaveform(phase, df, m1*lal.MSUN_SI, m2*lal.MSUN_SI, s1x, s1y, s1z, s2x, s2y, s2z, fmin, fmax, fref, dist*(1E6 * ls.lal.PC_SI), iota, lambda1, lambda2, waveFlags, nonGRparams, amp_order, phase_order, approx)
+	freqs=array([hplus_tilda.f0+i*hplus_tilda.deltaF for i in arange(hplus_tilda.data.length)])
+	return hplus_tilda.data.data,hcross_tilda.data.data,freqs
+
+#take the h+ component to calculate SNR
+def compute_horizonSNR(hplus_tilda,psd_interp,fsel,df):
+	return sqrt(4.*df*sum(abs(hplus_tilda[fsel])**2/psd_interp))        
+
+#calculate the horizon distance/redshift
+def find_horizon_range(m1,m2,asdfile,approx=ls.IMRPhenomD):
+
+	fmin=10.
+	fref=10.
+	df=1e-2
+	
+	input_freq,strain=loadtxt(asdfile,unpack=True)
+	minimum_freq=maximum(10.,min(input_freq))
+	maximum_freq=minimum(5000.,max(input_freq))
+	interpolate_psd = interp1d(input_freq, strain**2)	
+		
+	#initial guess of horizon redshift and luminosity distance
+	z0=0.1
+	input_dist=cd.luminosity_distance(z0,**cosmo)	
+	hplus_tilda,hcross_tilda,freqs= get_htildas((1.+z0)*m1,(1.+z0)*m2 ,input_dist,iota=0.,fmin=fmin,fref=fref,df=df,approx=approx)
+	fsel=logical_and(freqs>minimum_freq,freqs<maximum_freq)
+	psd_interp = interpolate_psd(freqs[fsel]) 	
+	input_snr=compute_horizonSNR(hplus_tilda,psd_interp,fsel,df)
+
+	input_redshift=z0; guess_snr=0    
+	#evaluate the horizon recursively
+	while abs(guess_snr-snr_th)>snr_th*0.001: #require the error within 0.1%
+		guess_redshift,guess_dist=horizon_dist_eval(input_dist,input_snr,input_redshift) #horizon guess based on the old SNR		
+		
+		#observer frame or source frame masses
+		hplus_tilda,hcross_tilda,freqs= get_htildas((1.+guess_redshift)*m1,(1.+guess_redshift)*m2 ,guess_dist,iota=0.,fmin=fmin,fref=fref,df=df,approx=approx)
+		fsel=logical_and(freqs>minimum_freq,freqs<maximum_freq)
+		psd_interp = interpolate_psd(freqs[fsel]) 		
+		guess_snr=compute_horizonSNR(hplus_tilda,psd_interp,fsel,df) #calculate the new SNR
+
+		input_snr=guess_snr
+		input_redshift=guess_redshift
+		input_dist=guess_dist
+	horizon_redshift=guess_redshift
+	
+	#sampled universal antenna power pattern for code sped up
+	w_sample,P_sample=genfromtxt("/Users/hsinyuc/research/followup/data/Pw_single.dat",unpack=True)
+	P=interp1d(w_sample, P_sample,bounds_error=False,fill_value=0.0)
+	n_zstep=100
+
+	z,dz=linspace(horizon_redshift,0,n_zstep,endpoint=False,retstep=True)
+	dz=abs(dz)
+	vol_sum=0
+	for i in range(0,size(z)):	
+		hplus_tilda,hcross_tilda,freqs = get_htildas((1.+z[i])*m1,(1.+z[i])*m2 ,cd.luminosity_distance(z[i],**cosmo) ,fmin=fmin,fref=fref,df=df,approx=approx)
+		fsel=logical_and(freqs>minimum_freq,freqs<maximum_freq)
+		psd_interp = interpolate_psd(freqs[fsel])  
+		optsnr_z=compute_horizonSNR(hplus_tilda,psd_interp,fsel,df)
+		w=snr_th/optsnr_z
+		vol_sum=vol_sum+(cd.comoving_volume(z[i]+dz/2.,**cosmo)-cd.comoving_volume(z[i]-dz/2.,**cosmo))/(1.+z[i])*P(w)
+	
+	range_2=(vol_sum*3/4./pi)**(1./3.)
+	range_redshift,res=leastsq(findzfromDC,z0,args=range_2)
+	range_3=cd.luminosity_distance(range_redshift[0],**cosmo)
+	
+	return horizon_redshift,cd.luminosity_distance(horizon_redshift,**cosmo), range_2, range_3, vol_sum
+
+m1=30.
+m2=30.
+approx=ls.IMRPhenomD
+asdfile = '/Users/hsinyuc/research/followup/data/2015-10-01_H1_O1_Sensitivity_strain_asd.txt'
+#asdfile='/Users/hsinyuc/research/followup/data/2016-12-13_C01_L1_O2_Sensitivity_strain_asd.txt'
+
+print find_horizon_range(m1,m2,asdfile,approx=approx)
